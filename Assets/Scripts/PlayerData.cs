3966f55592e02fbc62ba01f4d1a266055d6de70d
jerdmann94 <erdmannjacob0@gmail.com>
2023-07-07 20:19:33 -0500

    adding gitignore

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-   public int strength;
-   public int intelligence;
-   public int dexterity;
-   public int life;
+    public int strength;
+    public int intelligence;
+    public int dexterity;
+    public int life;
 }
